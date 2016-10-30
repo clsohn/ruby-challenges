dbb8338e8319841151caca06f4de8c6de3614281
Christine Sohn <christinesohn@Christines-MacBook-Air.local>
2016-10-30 14:30:07 -0400

    Fixed lovely wine

--- lovely_wine.rb
+++ lovely_wine.rb
@@ -1,3 +1,23 @@
+class Wine
+
+def set_name=(wine_name)
+@name = wine_name
+end
+
+def get_name
+return @name
+end
+
+def set_vineyard=(vineyard_name)
+@vineyard = vineyard_name
+end
+
+def get_vineyard
+return @vineyard
+end
+
+end
+
 my_wine = Wine.new
 my_wine.set_name="Cabernet"
 wine_name = my_wine.get_name
